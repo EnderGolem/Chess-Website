64595f335518aaa44977da1bcf9c85a2c0d61926
Artemtrue <artem-tishchenko-2002@mail.ru>
2022-12-26 16:52:02 +0300

    Registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
+
+  get '/register', to: 'register#new'
+  post '/register', to: 'register#create'
+
+  get '/login', to: 'login#new'
+  post '/login', to: 'login#create'
+  get '/logout', to: 'login#destroy'
+
+  get '/dashboard', to: 'dashboard#index'
 end
